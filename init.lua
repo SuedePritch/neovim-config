fa18e92fcdd4ccf9beac076e3006c645f0512257
James Pritchard <james.edward.pritchard@gmail.com>
2023-10-01 14:49:39 -0400

    runs the after folder on load

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
 require('suedepritch')
+-- Inside your init.lua file
+local fn = vim.fn
+local execute = vim.api.nvim_command
 
+local after_dir = fn.stdpath('config') .. '/after'
+
+if fn.isdirectory(after_dir) then
+  local after_files = fn.glob(after_dir .. '/*.lua', true, true)
+  for _, file in ipairs(after_files) do
+    execute('luafile ' .. fn.fnameescape(file))
+  end
+end
 
